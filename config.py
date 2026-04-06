0d06c01eff586ded03f527f5457563b40116e3c8
KAVINSARATHI <155992312+Kavinsarathi@users.noreply.github.com>
2026-04-06 13:00:14 +0530

    Update and rename config_example.py to config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+session_status_path = "data/session_status.json"
+session_state_store_path = "data/session.json"
+state_history_path = "data/history.json"
+
+base_url = 'https://www.linkedin.com/login/'
+target_url = 'https://www.linkedin.com/feed/'
+
+CTA_type='Fresher and Experience Question about their agent building'
+Length='180-220 words'
+tone='fresher with a specific example from your work'
+Audience='ML Engineers'
